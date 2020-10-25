2ae477cc2fa68e8a1ec39c554827ad0925c9aa17
Nemanja <nemanjakostovski@gmail.com>
2020-10-25 16:18:55 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import flask
+import pickle
+import pandas as pd
+
+with open(f'model/alloys_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = flask.Flask(__name__, template_folder = 'templates')
+
+@app.route('/', methods = ['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    if flask.request.method == 'POST':
+        max_strength = flask.request.form['Rm']
+        yield_strength = flask.request.form['Rp']
+        elongation = flask.request.form['A%']
+        webster = flask.request.form['Wb']
+        input_variables = pd.DataFrame([[max_strength, yield_strength, elongation, webster]],
+                                       columns = ['Rm', 'Rp', 'A%', 'Wb'],
+                                       dtype = float)
+        prediction = model.predict(input_variables)[0]
+        return flask.render_template('main.html',
+                                     original_input = {'Rm':max_strength,
+                                                     'Rp':yield_strength,
+                                                     'A%':elongation,
+                                                     'Wb':webster},
+                                     result = prediction
+                                     )
+if __name__ == '__main__':
+    app.run()
---
+++ model/model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import scikitplot as skplt
+import seaborn as sns
+
+df = pd.read_csv('Bazaproizvodnjaprofila1.csv')
+X = df[['Rm', 'Rp', 'A%', 'Wb', 'Hours in oven' ]].values
+y = df[['Alloy']].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2)
+
+model = LogisticRegression(max_iter=6500)
+model.fit(X_train, y_train.ravel())
+
+y_pred = model.predict(X_test)
+
+sns.relplot(data=df, x="Rm", y="A%", hue="Alloy", alpha=0.8)
+
+skplt.metrics.plot_confusion_matrix(
+    y_test, 
+    y_pred,
+    figsize=(12,12),
+    text_fontsize=20,
+    title_fontsize=20)
+
+import pickle
+with open('alloys_model.pkl', 'wb') as file:
+    pickle.dump(model, file)
