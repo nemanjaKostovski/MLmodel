50bfec20bf3644996bcaf326f9db0733f2cecd73
Nemanja <nemanjakostovski@gmail.com>
2021-04-21 13:45:57 +0200

    js

---
+++ static/main.js
@@ -0,0 +1,11 @@
+// Adding functionality to form submit
+
+const form = document.querySelector('.form');
+const result = document.querySelector('.result');
+
+form.addEventListener('onsubmit', showResult);
+
+function showResult(){
+    result.style.display = block;
+}
+
