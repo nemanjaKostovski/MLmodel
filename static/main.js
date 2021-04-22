4e42faf6ca03f6e14d5795dafb0755796a3e94c3
Nemanja <nemanjakostovski@gmail.com>
2021-04-22 10:48:45 +0200

    js

--- static/main.js
+++ static/main.js
@@ -3,7 +3,7 @@
 const button = document.querySelector('.button');
 const result = document.querySelector('.result');
 
-button.addEventListener('onsubmit', showResult);
+button.addEventListener('submit', showResult);
 
 function showResult(){
     result.style.display = 'block';
